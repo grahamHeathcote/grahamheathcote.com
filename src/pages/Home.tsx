8871c2c625d94034ce8b4dfdb5d5cef3133f647a
Graham Heathcote <grahamkheathcote@gmail.com>
2026-07-08 04:40:45 +0300

    diss

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
 
 function Home() {
-
   return (
     <div className="p-4">
       <h1 className="text-xl font-semibold mb-2">Graham Heathcote</h1>
-      <h2>
-        Welcome to my (soon to be improved) website.
-      </h2>
-      <Link className="mt-2 text-blue-500 hover:underline" to="/chess">In the meantime, play my chess engine. </Link>
+      <h2>Welcome to my (soon to be improved) website.</h2>
+      In the meantime,{" "}
+      <Link className="mt-2 text-blue-500 hover:underline" to="/chess">
+        {" "}
+        play my chess engine{" "}
+      </Link>
+      or{" "}
+      <a
+        className="mt-2 text-blue-500 hover:underline"
+        href="/pdf/undergraduate_dissertation.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        read my dissertation.
+      </a>
     </div>
   );
 }
