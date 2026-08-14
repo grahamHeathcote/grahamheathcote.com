266823d4107fd19eff8a644e6bcc10c26345cfd4
Graham Heathcote <grahamkheathcote@gmail.com>
2026-08-14 18:15:10 -0500

    github

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,20 +5,28 @@ function Home() {
     <div className="p-4">
       <h1 className="text-xl font-semibold mb-2">Graham Heathcote</h1>
       <h2>Welcome to my (soon to be improved) website.</h2>
-      In the meantime,{" "}
+      In the meantime, play my 
       <Link className="mt-2 text-blue-500 hover:underline" to="/chess">
         {" "}
-        play my chess engine{" "}
+        chess engine, {" "}
       </Link>
-      or{" "}
+      read my {" "}
       <a
         className="mt-2 text-blue-500 hover:underline"
         href="/pdf/undergraduate_dissertation.pdf"
         target="_blank"
         rel="noopener noreferrer"
       >
-        read my dissertation.
+        dissertation,{" "}
       </a>
+      or visit my
+      <Link
+        className="mt-2 text-blue-500 hover:underline"
+        to="https://github.com/grahamHeathcote"
+      >
+        {" "}
+        GitHub page.
+      </Link>
     </div>
   );
 }
